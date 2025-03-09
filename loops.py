65d035f3823e32bc387bc39c07e56254aa4aa7f1
Gustavo <gustavocoderjs@gmail.com>
2025-03-09 00:22:51 -0300

    Algorithm of loops added

---
+++ loops.py
@@ -0,0 +1,6 @@
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0, n): # Itera de zero até n-1 
+        print(i ** 2)  # Use: end=" " para imprimir os elementos na mesma linha
