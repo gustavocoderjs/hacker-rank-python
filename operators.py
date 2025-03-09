398425ac62f34de534216068d3d67d52bd88c2fb
Gustavo <gustavocoderjs@gmail.com>
2025-03-08 22:53:38 -0300

    File added to repository: operators.py

---
+++ operators.py
@@ -0,0 +1,9 @@
+
+if __name__ == '__main__':
+    a = int(input("Insert the first number: "))
+    b = int(input("Insert the second number: "))
+    
+print(f"The sum of the two numbers is: {a + b}")
+print(f"The difference of the two numbers is: {a - b}")
+print(f"The product of the two numbers is: {a * b}")
+
